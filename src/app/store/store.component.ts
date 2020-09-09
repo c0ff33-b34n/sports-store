4f6086b2a39c7a8fcb5cb86bb84d82678c0f71fe
Stuart Bowes <stuartbowes2000@gmail.com>
2020-09-09 10:49:00 +0100

    Add pagination to products list in store component

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,11 +9,14 @@ import { Product } from '../model/product.model';
 })
 export class StoreComponent {
   public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private productRepo: ProductRepository) { }
 
   get products(): Product[] {
-    return this.productRepo.getProducts(this.selectedCategory);
+    const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.productRepo.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
@@ -23,4 +26,19 @@ export class StoreComponent {
   changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory;
   }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.productRepo.getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0)
+      .map((x, i) => i + 1);
+    }
 }
