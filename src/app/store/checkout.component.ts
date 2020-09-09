137ac2f483bff98d61c8723ca978c61cd13bb9c8
Stuart Bowes <stuartbowes2000@gmail.com>
2020-09-09 16:23:11 +0100

    Add checkout component details with address form

--- src/app/store/checkout.component.ts
+++ src/app/store/checkout.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderRepository } from '../model/order.repository';
+import { Order } from '../model/order.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent {
+  orderSent = false;
+  submitted = false;
 
-  constructor() { }
+  constructor(public orderRepo: OrderRepository,
+              public order: Order) {}
 
-  ngOnInit(): void {
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if (form.valid) {
+      this.orderRepo.saveOrder(this.order).subscribe(
+        order => {
+          this.order.clear();
+          this.orderSent = true;
+          this.submitted = false;
+        }
+      );
+    }
   }
-
 }
