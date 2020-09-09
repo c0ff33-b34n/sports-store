8fde66b1f404e62d08785613acb7f8316bd8b18b
Stuart Bowes <stuartbowes2000@gmail.com>
2020-09-09 15:35:24 +0100

    Add details to cart detail component

--- src/app/store/cart-detail.component.ts
+++ src/app/store/cart-detail.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../model/cart.model';
 
 @Component({
   selector: 'app-cart-detail',
   templateUrl: './cart-detail.component.html',
   styleUrls: ['./cart-detail.component.scss']
 })
-export class CartDetailComponent implements OnInit {
+export class CartDetailComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(public cart: Cart) { }
 
 }
