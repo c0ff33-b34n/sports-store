efe0c821c890c84890ca57dd6919f3e70d286a48
Stuart Bowes <stuartbowes2000@gmail.com>
2020-09-10 15:14:12 +0100

    Change Web Service URL

--- src/app/rest.datasource.ts
+++ src/app/rest.datasource.ts
@@ -15,7 +15,7 @@ export class RestDatasource {
     authToken: string;
 
     constructor(private http: HttpClient) {
-        this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+        this.baseUrl = 'api';
     }
 
     getProducts(): Observable<Product[]> {
