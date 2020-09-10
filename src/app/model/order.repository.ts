f329f9e4323e03a2458000a3b966876b3514ad7a
Stuart Bowes <stuartbowes2000@gmail.com>
2020-09-10 11:20:56 +0100

    Add CRUD operations to datasource, product and order repos

--- src/app/model/order.repository.ts
+++ src/app/model/order.repository.ts
@@ -1,20 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Order } from './order.model';
-import { StaticDataSource } from './static.datasource';
 import { Observable } from 'rxjs';
+import { RestDatasource } from '../rest.datasource';
 
 
 @Injectable()
 export class OrderRepository {
     private orders: Order[] = [];
+    private loaded = false;
 
-    constructor(private dataSource: StaticDataSource) { }
+    constructor(private dataSource: RestDatasource) { }
+
+    loadOrders() {
+        this.loaded = true;
+        this.dataSource.getOrders().subscribe(orders => this.orders = orders);
+    }
 
     getOrders(): Order[] {
+        if (!this.loaded) {
+            this.loadOrders();
+        }
         return this.orders;
     }
 
     saveOrder(order: Order): Observable<Order> {
         return this.dataSource.saveOrder(order);
+    }
+    updateOrder(order: Order) {
+        this.dataSource.updateOrder(order).subscribe(ord => {
+            this.orders.splice(this.orders.findIndex(o => o.id === order.id), 1, order);
+        });
+    }
+
+    deleteOrder(id: number) {
+        this.dataSource.deleteOrder(id).subscribe(ord => {
+            this.orders.splice(this.orders.findIndex(o => o.id === id), 1);
+        });
     }
 }
