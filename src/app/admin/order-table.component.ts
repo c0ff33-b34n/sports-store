5607278894a4ba0f0349d3244972893e7ad371d5
Stuart Bowes <stuartbowes2000@gmail.com>
2020-09-10 13:53:05 +0100

    Implement the admin orders page

--- src/app/admin/order-table.component.ts
+++ src/app/admin/order-table.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderRepository } from '../model/order.repository';
+import { Order } from '../model/order.model';
 
 @Component({
   selector: 'app-order-table',
   templateUrl: './order-table.component.html',
   styleUrls: ['./order-table.component.scss']
 })
-export class OrderTableComponent implements OnInit {
+export class OrderTableComponent {
+  includeShipped = false;
 
-  constructor() { }
+  constructor(private orderRepo: OrderRepository) { }
 
-  ngOnInit(): void {
+  getOrders(): Order[] {
+    return this.orderRepo.getOrders()
+      .filter(o => this.includeShipped || !o.shipped);
   }
 
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.orderRepo.updateOrder(order);
+  }
+
+  delete(id: number) {
+    this.orderRepo.deleteOrder(id);
+  }
 }
